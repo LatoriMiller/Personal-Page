5f986afa99047c1c9e08f8d61a932ad7152b4bdc
LatoriMiller <latori.miller@gmail.com>
2019-01-27 21:10:16 -0500

    first commit

---
+++ src/main/java/com/techtalentsouth/latoriHeroku/LatoriHerokuApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.latoriHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatoriHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatoriHerokuApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/techtalentsouth/latoriHeroku/LatoriHerokuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.latoriHeroku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LatoriHerokuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
